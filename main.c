e946823da057bcdcb2d95d6da8bd33e47dd3f964
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2019-09-12 09:19:02 -0700

    Pythagoras lab
    here is my Pythagoras lab

--- main.c
+++ main.c
@@ -1,51 +1,28 @@
 //
 //  main.c
-//  datatypeslab
+//  Pythagoras Lab
 //
-//  Created by Bulkin, Lola on 9/12/19.
+//  Created by Bulkin, Lola on 9/10/19.
 //  Copyright © 2019 Bulkin, Lola. All rights reserved.
 //
 
 #include <stdio.h>
-    
-int main()
-    {
-        int arr1[100];
-        int i, mx, mn, n;
-        
-        printf("Input the number of elements that you want to be stored in the array:");
-        scanf("%d",&n);
-        
-        printf("Input %d elements in the array :\n",n);
-        for(i=0;i<n;i++)
-        {
-            printf("element number %d : ",i);
-            scanf("%d",&arr1[i]);
-        }
-        
-        
-        mx = arr1[0];
-        mn = arr1[0];
-        
-        for(i=1; i<n; i++)
-        {
-            if(arr1[i]>mx)
-            {
-                mx = arr1[i];
-            }
-            
-            
-            if(arr1[i]<mn)
-            {
-                mn = arr1[i];
-            }
-        }
-        printf("According to your inputs, the maximum element is : %d\n", mx);
-        printf("According to your inputs, the minimum element is : %d\n\n", mn);
-        
-        return 0;
-    }
+#include <math.h>
 
+int main(int argc, const char * argv[]) {
+    
+    double side1;
+    double side2;
+    printf("Hello, we will help you do pythagorean theorum!");
+    printf("what is side1");
+    scanf("%lf",&side1);
+    printf("what is side2");
+    scanf("%lf",&side2);
+    double side3 = sqrt(side1*side1 + side2*side2);
+    printf ("the hypotenuse is %lf",side3);
+    
+    return 0;
+}
 
 
 
