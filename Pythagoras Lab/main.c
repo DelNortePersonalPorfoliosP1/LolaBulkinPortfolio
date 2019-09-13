5d035cc0b85dbee68fbc11ef9836171264cdff0c
Lola Bulkin <lolabulkin@gmail.com>
2019-09-13 08:49:20 -0700

    start

---
+++ Pythagoras Lab/main.c
@@ -0,0 +1,28 @@
+//
+//  main.c
+//  Pythagoras Lab
+//
+//  Created by Bulkin, Lola on 9/10/19.
+//  Copyright © 2019 Bulkin, Lola. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    
+    double side1;
+    double side2;
+    printf("Hello, we will help you do pythagorean theorum!");
+    printf("what is side1");
+    scanf("%lf",&side1);
+    printf("what is side2");
+    scanf("%lf",&side2);
+    double side3 = sqrt(side1*side1 + side2*side2);
+    printf ("the hypotenuse is %lf",side3);
+    
+    return 0;
+}
+
+
+
