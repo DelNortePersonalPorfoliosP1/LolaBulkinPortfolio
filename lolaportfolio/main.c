95fc76932f604791af6f5260f86199d0580f5ff4
Lola Bulkin <lolabulkin@gmail.com>
2019-09-19 08:42:42 -0700

    finish

--- lolaportfolio/main.c
+++ lolaportfolio/main.c
@@ -8,18 +8,10 @@
 
 #include <stdio.h>
 #include <math.h>
-
+#include "pythagoras.h"
 int main(int argc, const char * argv[]) {
     
-    double side1;
-    double side2;
-    printf("Hello, we will help you do pythagorean theorum!");
-    printf("what is side1");
-    scanf("%lf",&side1);
-    printf("what is side2");
-    scanf("%lf",&side2);
-    double side3 = sqrt(side1*side1 + side2*side2);
-    printf ("the hypotenuse is %lf",side3);
+    pythagoras();
     
     return 0;
 }
