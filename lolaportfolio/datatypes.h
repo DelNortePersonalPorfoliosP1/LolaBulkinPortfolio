19d19c2f70783a785c0a158375052d557550bc06
lolabulkin <54717937+lolabulkin@users.noreply.github.com>
2019-09-19 08:52:22 -0700

    Add files via upload

---
+++ lolaportfolio/datatypes.h
@@ -0,0 +1,14 @@
+//
+//  datatypes.h
+//  Pythagoras Lab
+//
+//  Created by Bulkin, Lola on 9/17/19.
+//  Copyright © 2019 Bulkin, Lola. All rights reserved.
+//
+
+#ifndef datatypes_h
+#define datatypes_h
+
+#include <stdio.h>
+
+#endif /* datatypes_h */
