22674abe3b9fdf04534b21c564eefa390bccdd9e
Lola Bulkin <lolabulkin@gmail.com>
2019-09-20 09:04:17 -0700

    committing with menu

---
+++ datatypes.h
@@ -0,0 +1,15 @@
+//
+//  datatypes.h
+//  lolaportfolio
+//
+//  Created by Bulkin, Lola on 9/20/19.
+//  Copyright © 2019 Bulkin, Lola. All rights reserved.
+//
+
+#ifndef datatypes_h
+#define datatypes_h
+
+#include <stdio.h>
+int datas(void);
+
+#endif /* datatypes_h */
--- main.c
+++ main.c
@@ -9,9 +9,21 @@
 #include <stdio.h>
 #include <math.h>
 #include "pythagoras.h"
+#include "datatypes.h"
 int main(int argc, const char * argv[]) {
     
+    int a;
+    printf("what lab do u want to run? choose 1 or 2");
+    scanf("%i",&a);
+    if (a==1){
     pythagoras();
+    }
+    else if (a==2){
+    datas();
+    }
+    else {
+        printf ("not valid response");
+    }
     
     return 0;
 }
